64b55d46fff061ec6641d84fdcb077fc162c477d
electron <labdul_qadir@st.ug.edu.gh>
2020-10-12 18:05:25 +0000

    imports optimized

--- test/com/samaritan/util/Base64DataFieldSetterTest.java
+++ test/com/samaritan/util/Base64DataFieldSetterTest.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Base64DataFieldSetterTest {
 
--- test/com/samaritan/util/Base64UtilTest.java
+++ test/com/samaritan/util/Base64UtilTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class Base64UtilTest {
 
